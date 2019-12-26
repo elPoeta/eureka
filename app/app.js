bd49de6405cd0631d6e6aad011ff7b7f12e75484
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2019-12-26 13:59:09 -0300

    add stop when out last number

--- app/app.js
+++ app/app.js
@@ -1,20 +1,27 @@
 console.log("EUREKA - BINGO");
 import Lottery from './lottery.js';
-const lottery = new Lottery({totalNumbers: 99}).shuffleNumbers();
+const totalNumbers = 3;
+const lottery = new Lottery({totalNumbers}).shuffleNumbers();
 console.log(lottery.getShuffledNumbers());
 const luckyNumber = document.querySelector('#number');
 const machineCage = document.querySelector('#machineCage');
 const spin = document.querySelector('.spin');
 const numbers = document.querySelector('.numbers');
-
+let tableOfNumbers = [];
+let numberShift
 const showNumber = e => {
+  
   if(!spin.classList.contains('hidden')) {
-    luckyNumber.innerHTML = lottery.getNumber();
-    numbers.classList.remove('hidden');
-    spin.classList.add('hidden');
-  } else {
-    numbers.classList.add('hidden');
-    spin.classList.remove('hidden');
+       numberShift = lottery.getNumber();
+      if(typeof(numberShift) == 'number') {
+        tableOfNumbers.push(numberShift);
+        luckyNumber.innerHTML = numberShift
+        numbers.classList.remove('hidden');
+        spin.classList.add('hidden');
+      }
+  } else if(tableOfNumbers.length-1 < totalNumbers){
+      numbers.classList.add('hidden');
+      spin.classList.remove('hidden');
   }
 }
 
